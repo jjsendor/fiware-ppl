493c16b355921b16b65c1568b11fd2df90a19e49
Jakub Sendor <jakub.sendor@sap.com>
2014-04-02 16:18:11 +0200

    Added main PDP interface method

---
+++ pdp/src/main/java/com/sap/a4cloud/apple/pdp/request/PdpRequest.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2014, SAP AG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     - Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *     - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *     - Neither the name of the SAP AG nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+package com.sap.a4cloud.apple.pdp.request;
+
+/**
+ * Authorization request used by decision point during the policy evaluation.
+ *
+ * @author Jakub Sendor
+ *
+ */
+public class PdpRequest {
+
+	private String subject;
+	private String resource;
+	private String purpose;
+
+	/**
+	 * Create request for a given subject, resource and purpose.
+	 *
+	 * @param subject	the subject requesting authorization
+	 * @param resource	the resource to which access is requested
+	 * @param purpose	the purpose of requesting authorization to access
+	 * 					the resource
+	 */
+	public PdpRequest(String subject, String resource, String purpose) {
+		super();
+		this.subject = subject;
+		this.resource = resource;
+		this.purpose = purpose;
+	}
+
+	/**
+	 * Returns the subject which is requesting the authorization.
+	 *
+	 * @return	the request subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+	
+	/**
+	 * Returns the resource to which access is requested.
+	 *
+	 * @return	the request resource
+	 */
+	public String getResource() {
+		return resource;
+	}
+
+	/**
+	 * Returns the purpose for which requested resource is accessed.
+	 *
+	 * @return the request purpose
+	 */
+	public String getPurpose() {
+		return purpose;
+	}
+
+}
